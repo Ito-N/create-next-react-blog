5a66ac87e058c7deab6568f7fc348ed3f9ea8d2a
Ito-N <theme4087abcdef@gmail.com>
2022-08-11 09:19:28 +0900

    date-fnsインストールして日付変換

---
+++ components/convert-date.js
@@ -0,0 +1,10 @@
+import { format, parseISO } from "date-fns";
+import { ja } from "date-fns/locale";
+
+export default function ConvertDate({ dateISO }) {
+  return (
+    <time dateTime={dateISO}>
+      {format(parseISO(dateISO), "yyyy年MM月dd日", { locale: ja })}
+    </time>
+  );
+}
